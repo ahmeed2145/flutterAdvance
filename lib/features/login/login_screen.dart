c97ad5cc2d873635b95b10e6536b0131f3e678bf
Ahmed khaled <mazen00mazen003@gmail.com>
2025-07-28 00:43:07 +0300

    making dev  branch

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -4,7 +4,7 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
